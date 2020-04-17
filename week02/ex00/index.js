dbfab6593bbd61eff25314381c44004defe5cf7a
Rubi Kina <rkina@dindon.com>
2020-04-17 19:36:59 +0300

    add week02:

---
+++ week02/ex00/index.js
@@ -0,0 +1,62 @@
+/**
+ * @param {String} date
+ * @returns {Object}
+ */
+
+ function getFormat(date) {
+    var format = date.toISOString();
+
+    format = format.replace(/(T)/, ' ').replace(/(:.{5}[0-9]Z)/, '');
+    return format;
+ }
+
+function checkCorrect(value, name) {
+    var number = parseInt(value);
+    var names = ['years', 'months', 'days', 'hours', 'minutes'];
+
+    if (number < 0 || !names.includes(name))
+        throw new TypeError('Передано неверное значение');
+}
+function changeDate(value, name, date, sign) {
+    checkCorrect(value, name);
+    value = sign === '-' ? value * (-1) : value;
+    switch(name) {
+        case 'years':
+            date.setFullYear(date.getFullYear() + value);
+            break;
+        case 'months':
+            date.setMonth(date.getMonth() + value);
+            break;
+        case 'days':
+            date.setDate(date.getDate() + value);
+            break;
+        case 'hours':
+            date.setHours(date.getHours() + value);
+            break;
+        case 'minutes':
+            date.setMinutes(date.getMinutes() + value);
+            break;
+    }
+    return date;
+}
+
+module.exports = function (date) {
+    date = new Date(date);
+    date.setHours(date.getHours() + 3);
+
+    var time = {
+        get value() {
+            return getFormat(date);
+        },
+        add: function (value, name) {
+            changeDate(value, name, date, '+');
+            return this;
+        },
+        subtract: function (value, name) {
+            changeDate(value, name, date, '-');
+            return this;
+        }
+    };
+    
+    return time;
+};
